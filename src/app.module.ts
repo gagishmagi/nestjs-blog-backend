be9df4400216413b6717662463089dce2050a478
bresleveloper <ariel.rubi@gmail.com>
2020-11-23 18:38:37 +0200

    Fixed issue with app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,13 @@ import { AppService } from './app.service';
 import { DatabaseConnectionService } from './database-connection/database-connection.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ProfileController } from './profile/profile.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({
     useClass: DatabaseConnectionService
   }), AuthModule, UserModule],
-  controllers: [AppController, ProfileController],
+  controllers: [AppController],
   providers: [AppService, DatabaseConnectionService],
 })
 export class AppModule {}
